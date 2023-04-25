ab15b5a24d348a903f771046e62b7e7f88e7cbb4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-25 10:27:58 -0300

    add interface segregatation principle with shapes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,45 @@
+interface ShapeInterface {
+    public double area();
+}
+
+interface SolidShapeInterface {
+    public double volume();
+}
+
+interface ManageShapeInterface {
+    public double calculate();
+}
+
+class Cube implements ShapeInterface, SolidShapeInterface, ManageShapeInterface {
+
+    @Override
+    public double area() {
+        return 1.0;
+    }
+
+    @Override
+    public double volume() {
+        return 1.0;
+    }
+
+    @Override
+    public double calculate() {
+        return this.area() + this.volume();
+    }
+}
+
+class Square implements ShapeInterface, ManageShapeInterface {
+    @Override
+    public double area() {
+        return 1.0;
+    }
+
+    @Override
+    public double calculate() {
+        return this.area();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
